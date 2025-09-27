418818cb50691a1558e6ca6f21c4e3c1b446d931
Hoang Ngoc Lam LE <122269400+lamlhn@users.noreply.github.com>
2025-09-27 17:22:12 +0200

    Update pyy.py

--- pyy.py
+++ pyy.py
@@ -1,5 +1,8 @@
 def ex_fonction(name):
 	print(f"{name} is here")
 
-ex_fonction("Lam")
+def call_function(name, age):
+	ex_fonction(name)
+	print(f"and {name} is {age}")
 
+call_function("Lam", 23)
